9656c2d5db4cb3af193c31a6f760577d9369e26a
ferddymalemo08 <fmalemo@jkuat.ac.ke>
2023-01-11 05:15:30 -0800

    sets and dictionaries

---
+++ 0x04-python-more_data_structures/run_all.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+python3 0-main.py
+python3 1-main.py
+python3 2-main.py
+python3 3-main.py
+python3 4-main.py
+python3 5-main.py
+python3 6-main.py
+python3 7-main.py
+python3 8-main.py
+python3 9-main.py
+python3 10-main.py
+python3 11-main.py
+python3 12-main.py
+python3 100-main.py
+python3 101-main.py
+python3 102-main.py
