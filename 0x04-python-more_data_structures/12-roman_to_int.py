b2790542aaf80bfef089facacd23fc5b320876d6
ferddymalemo08 <fmalemo@jkuat.ac.ke>
2023-01-11 05:43:36 -0800

    sets and dictionaries

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,15 +1,14 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    if roman_string is None or isinstance(roman_string, str) is False:
+    if roman_string and type(roman_string) is str:
+        dictn = dict(I=1, V=5, X=10, L=50, C=100, D=500, M=1000)
+        value = 0
+        for a, x in zip(roman_string, roman_string[1:]):
+            if dictn[a] < dictn[x]:
+                value -= dictn[a]
+            else:
+                value += dictn[a]
+        value += dictn[roman_string[-1]]
+        return value
+    else:
         return 0
-    letters = {'I': 1, 'V': 5,  'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
-    reverse_num = roman_string[::-1]
-    maxchar = 'I'
-    total = 0
-    for i in reverse_num:
-        if letters[i] >= letters[maxchar]:
-            maxchar = i
-            total += letters[i]
-        else:
-            total -= letters[i]
-    return total
